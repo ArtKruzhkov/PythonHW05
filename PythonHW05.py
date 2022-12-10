4cd7d710a01940581d2b664500048e634919f74e
Artem <artkruzhkov@gmail.com>
2022-12-10 18:30:58 +0300

    firsttaskHW05

---
+++ PythonHW05.py
@@ -0,0 +1,47 @@
+# Создайте программу для игры с конфетами человек против компьютера.
+# Условие задачи: На столе лежит 150 конфет. Играют игрок против компьютера. Первый ход определяется жеребьёвкой.
+# За один ход можно забрать не более чем 28 конфет.
+# from random import randint
+
+# def input_candy(name):
+#     x = int(input(f"{name}, введите количество конфет, которое возьмете от 1 до 28: "))
+#     while x < 1 or x > 28:
+#         x = int(input(f"{name}, введите корректное количество конфет: "))
+#     return x
+
+# def p_print(name, amount, counter, value):
+#     print(f"Ходил {name}, он взял {amount}, теперь у него {counter}. Осталось на столе {value} конфет.")
+
+# player1 = input("Введите имя первого игрока: ")
+# player2 = input("Введите имя второго игрока: ")
+# value = int(input("Введите количество конфет на столе: "))
+
+# flag = randint(0,2)                            # флаг очередности
+# if flag:
+#     print(f"Первый ходит {player1}")
+# else:
+#     print(f"Первый ходит {player2}")
+
+# counter1 = 0 
+# counter2 = 0
+
+# while value > 28:
+#     if flag:
+#         k = input_candy(player1)
+#         counter1 += k
+#         value -= k
+#         flag = False
+#         p_print(player1, k, counter1, value)
+#     else:
+#         k = randint(1, 29)                    # сколько возьмет компьютер
+#         counter2 += k
+#         value -= k
+#         flag = True
+#         p_print(player2, k, counter2, value)
+
+# if flag:
+#     print(f"Выиграл {player1}")
+# else:
+#     print(f"Выиграл {player2}")
+
+
